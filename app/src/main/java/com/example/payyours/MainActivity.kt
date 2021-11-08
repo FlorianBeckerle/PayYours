088432e4370248183ca8a99215cb9ad2dd3006c7
Xodeen <f.beckerle@abc123.co.at>
2021-11-08 10:41:23 +0100

    New Porject
    Added new Project Files
    Implemented Settings - Files

---
+++ app/src/main/java/com/example/payyours/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.payyours
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item?.itemId){
+            R.id.menu_item_quit -> {
+                finish()
+                true
+            }
+            R.id.menu_item_settings ->{
+                Log.d("MainActivity", "Settings menu called")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
