f929d09937252ccc6bede4ab57ed37ddd942f4ee
Xodeen <f.beckerle@abc123.co.at>
2021-11-15 08:37:43 +0100

    Open Settings Activity
    Settings Activity can now be opened

--- app/src/main/java/com/example/payyours/MainActivity.kt
+++ app/src/main/java/com/example/payyours/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.payyours
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.menu_item_settings ->{
                 Log.d("MainActivity", "Settings menu called")
+                startActivity(Intent(this, SettingsActivity::class.java))
                 true
             }
             else -> super.onOptionsItemSelected(item)
