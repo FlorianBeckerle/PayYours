f07ee924a51bd32e3adad87f9894580f51694cd3
Xodeen <f.beckerle@abc123.co.at>
2021-11-15 09:44:44 +0100

    MainActivity
    Added MainActivity UI

--- app/src/main/java/com/example/payyours/SettingsActivity.kt
+++ app/src/main/java/com/example/payyours/SettingsActivity.kt
@@ -2,10 +2,16 @@ package com.example.payyours
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        buttonOk.setOnClickListener{ onSave() }
+    }
+
+    private fun onSave() {
+        finish()
     }
 }
